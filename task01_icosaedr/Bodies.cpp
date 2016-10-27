fb180e3036f657b13b0433048a06ec7cbbb4500d
Georgy Biryukov <intmpg@gmail.com>
2016-10-27 20:07:07 +0300

    icosahedronics without lighting

--- task01_icosaedr/Bodies.cpp
+++ task01_icosaedr/Bodies.cpp
@@ -108,7 +108,7 @@ void CIcosahedron::Draw() const
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	glEnable(GL_CULL_FACE);
-	glCullFace(GL_FRONT);
+	glCullFace(GL_BACK);
 
 	glBegin(GL_TRIANGLES);
 	OutputFaces();
